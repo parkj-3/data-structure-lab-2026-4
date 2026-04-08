e32675804571d67e29686db0b2a904a7158b59eb
parkj-3 <junjae7303@naver.com>
2026-04-08 17:59:04 +0900

    04-08-1800

--- Lec04_Array_Class/Car_Class/car.h
+++ Lec04_Array_Class/Car_Class/car.h
@@ -24,3 +24,16 @@ class Car {
 		}
 		void whereAmI() { printf("객체 주소 = %p\n", this); }
 };
+// 스포츠카 클래스 (자식 클래스) : 자동차 클래스에 터보 기능 추가
+class SportsCar : public Car
+{
+public:
+	using Car::Car;
+
+	bool bTurbo; // 터보 장치 ON?
+	void setTurbo(bool bTur) { bTurbo = bTur; }
+	void speedUp() { // 터보가 ON이 되어 있으면 가속이 빨리됨
+		if (bTurbo) speed += 20;
+		else Car::speedUp();
+	}
+};
--- Lec04_Array_Class/Car_Class/cartest.cpp
+++ Lec04_Array_Class/Car_Class/cartest.cpp
@@ -1,22 +1,61 @@
-#include <cstring>
 #include <cstdio>
+#include <cstring>
 #include "car.h"
-#include "SportsCar.h"
 
 int main() {
-    Car c1(50, "Avante", 3);
-    SportsCar sc;
+    int s, g, turbo;
+    char n[40];
 
-    sc.setTurbo(true);
-    sc.changeGear(3);
-    sc.speedUp();
-    sc.display();
+    // 1. 사용자로부터 값 입력받기
+    printf("자동차 이름 입력: ");
+    // scanf_s에서 %s를 쓸 때는 뒤에 사이즈(sizeof(n))를 꼭 넣어줘야 합니다.
+    scanf_s("%s", n, (unsigned int)sizeof(n));
 
+    printf("초기 속도 입력: ");
+    scanf_s("%d", &s);
+
+    printf("초기 기어 입력: ");
+    scanf_s("%d", &g);
+
+    printf("터보를 켜시겠습니까? (1: On, 0: Off): ");
+    scanf_s("%d", &turbo);
+
+    // 2. 입력받은 값으로 객체 생성
+    Car c1(s, n, g);      // 일반 자동차
+    SportsCar sc(s, n, g);         // 기본 생성자로 생성된 스포츠카 (이름 "Car", 속도 0)
+    
+    // 3. 실행 로직
+    printf("\n--- 실행 결과 ---\n");
+
+    // 일반 차 가속
+    printf("[일반 가속 전] ");
     c1.display();
-    c1.speedUp();
-    c1.changeGear(4);
+    c1.speedUp();         // 5 증가
+    printf("[일반 가속 후] ");
     c1.display();
-    c1.whereAmI();
 
+    // 스포츠카 가속 (터보 작동)
+    if (turbo == 1) {
+        sc.setTurbo(true);
+        printf("\n[스포츠카 터보 ON]\n");
+        
+        sc.speedUp();         // 20 증가 (터보 기능)
+        sc.display();
+
+        printf("\n--- 메모리 주소 ---\n");
+        c1.whereAmI();
+
+    }
+    else {
+        sc.setTurbo(false);
+        printf("\n[스포츠카가 터보 OFF]\n");
+      
+        sc.speedUp();         // 20 증가 (터보 기능)
+        sc.display();
+
+        printf("\n--- 메모리 주소 ---\n");
+        c1.whereAmI();
+    }
+         
     return 0;
 }
